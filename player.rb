f2fa19d46849c217d9a7ba204cff9ba4fe66a9b9
Valerie Dryden <outragedpinkracoon@gmail.com>
2024-11-04 20:31:00 +0000

    adds attributes

--- player.rb
+++ player.rb
@@ -2,12 +2,35 @@
 
 # Represents a Player in the game
 class Player
-  attr_accessor :name, :world, :life_form, :type
+  attr_accessor :name, :world, :life_form, :type, :story
+  attr_reader :attributes
 
-  def initialize(name:, world:, life_form:, type:)
+  def initialize(name:, world:, life_form:, type:, story:, attributes:)
     @name = name
     @world = world
     @life_form = life_form
     @type = type
+    @story = story
+    @attributes = attributes
+  end
+end
+
+class Attributes
+  class StatsError < StandardError; end
+
+  attr_reader :str, :dex, :con, :int, :wis, :cha
+  MAX_POINTS = 6
+
+  def initialize(str:, dex:, con:, int:, wis:, cha:)
+    if (str + dex + con + int + wis + cha != 6)
+      raise StatsError, "Stats must add up to 6"
+    end
+
+    @str = str
+    @dex = dex
+    @con = con
+    @int = int
+    @wis = wis
+    @cha = cha
   end
 end
--- player_test.rb
+++ player_test.rb
@@ -6,7 +6,21 @@ require_relative 'player'
 # Tests the Player class
 class PlayerTest < Minitest::Test
   def setup
-    @player = Player.new(name: 'fred', world: 'deans', life_form: 'human', type: 'gunslinger')
+    @player = Player.new(
+      name: 'fred', 
+      world: 'deans', 
+      life_form: 'human', 
+      type: 'gunslinger',
+      story: 'this is my story',
+      attributes: Attributes.new(
+        str: 1,
+        dex: 0,
+        con: 2,
+        int: 3,
+        wis: 0,
+        cha: 0
+      )
+    )
   end
 
   def test_player_has_name
@@ -31,5 +45,20 @@ class PlayerTest < Minitest::Test
     assert_equal @player.type, 'gunslinger'
     @player.type = 'wizard'
     assert_equal @player.type, 'wizard'
+  end
+
+  def test_player_has_story
+    assert_equal @player.story, 'this is my story'
+    @player.story = 'this is my new story'
+    assert_equal @player.story, 'this is my new story'
+  end
+
+  def test_player_attributes
+    assert_equal @player.attributes.str, 1
+    assert_equal @player.attributes.dex, 0
+    assert_equal @player.attributes.con, 2
+    assert_equal @player.attributes.int, 3
+    assert_equal @player.attributes.wis, 0
+    assert_equal @player.attributes.cha, 0
   end
 end
