edfd28901a0284ba56ec7732ed584aef8b22d8f3
Valerie Dryden <outragedpinkracoon@gmail.com>
2024-11-05 20:41:55 +0000

    adds hard coded scores for efforts

--- lib/player_efforts.rb
+++ lib/player_efforts.rb
@@ -2,9 +2,13 @@
 
 # The efforts for a given player
 class PlayerEfforts
-  attr_reader :basic
+  attr_reader :basic, :weapons_and_tools, :guns, :energy_and_magic, :ultimate
 
-  def initialize(basic:)
-    @basic = basic
+  def initialize
+    @basic = 4
+    @weapons_and_tools = 6
+    @guns = 8
+    @energy_and_magic = 10
+    @ultimate = 12
   end
 end
--- test/player_efforts_test.rb
+++ test/player_efforts_test.rb
@@ -5,11 +5,19 @@ require_relative '../lib/player_efforts'
 
 # Tests the PlayerEfforts class
 class PlayerEffortsTest < Minitest::Test
-  def test_valid_efforts
-    @efforts = PlayerEfforts.new(
-      basic: 6
-    )
+  def test_efforts
+    @efforts = PlayerEfforts.new
 
-    assert_equal(6, @efforts.basic)
+    expected_values = {
+      basic: 4,
+      weapons_and_tools: 6,
+      guns: 8,
+      energy_and_magic: 10,
+      ultimate: 12
+    }
+
+    expected_values.each do |effort_type, score|
+      assert_equal(score, @efforts.send(effort_type))
+    end
   end
 end
